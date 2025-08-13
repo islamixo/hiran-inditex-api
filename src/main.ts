da034b1ca9ce884102bbc3237fe812970e7a64cb
Hiran Del Castillo <hirandelcastillo@gmail.com>
2025-08-13 16:47:37 +0200

    Swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Image Tasks API')
+    .setDescription('REST API for image processing and task query')
+    .setVersion('1.0.0')
+    .build();
+  const doc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, doc);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
